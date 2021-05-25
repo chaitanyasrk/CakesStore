accaf9d9bd95cde8a0fca52f16815c96a4738a3d
Chaitanya SRK <chaitanya.nidadavole@gmail.com>
2021-05-25 23:01:10 +0530

    Security, Navigation and data manipulation functionality
    Added features like:
    1. Security - Basic Authorisation
    2. Data Manipulation - Add/Edit features
    3. Routing and Model validation
    4. Dependency Inject in Views and Models

---
+++ CakesStore/Pages/Account/Profile.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CakesStore.Pages.Account
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
